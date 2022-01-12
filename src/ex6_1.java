c8e9011de028e3d4975ec1620a80d29406c9e76d
khg339 <khg33333339@gmail.com>
2022-01-12 14:50:37 +0900

    위에서 아래로 구현

--- src/ex6_1.java
+++ src/ex6_1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class ex6_1 { //숫자 입력받고 내림차순으로 정렬하기
+public class ex6_1 {  //숫자 입력받고 내림차순으로 정렬하기
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
