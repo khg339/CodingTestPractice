c62f77f43a0344c1850d1972ed59384e29ac5d9f
khg339 <khg33333339@gmail.com>
2022-01-23 16:57:41 +0900

    전보 구현

--- src/Graph/ex10_2.java
+++ src/Graph/ex10_2.java
@@ -1,87 +1,104 @@
 package Graph;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class ex10_2 {
+    static class Node implements Comparable<Node>{ //노드 정보
+        private int index;    //노드번호
+        private int distance; //노드 까지의 거리
 
-    static int[] parent; //부모노드
-
-    static class Edge{ //간선
-
-        private int home1;
-        private int home2;
-        private int cost;
-
-
-        public Edge(int home1, int home2, int cost) {
-            this.home1 = home1;
-            this.home2 = home2;
-            this.cost = cost;
+        public Node(int index, int distance) {
+            this.index = index;
+            this.distance = distance;
         }
 
-        public int getHome1() {
-            return home1;
+        public int getIndex() {
+            return index;
         }
-        public int getHome2() {
-            return home2;
+
+        public int getDistance() {
+            return distance;
         }
-        public int getCost() {
-            return cost;
+
+        @Override //우선 순위 설정
+        public int compareTo(Node other){
+            return this.distance - other.distance;
+            //더 작은 값이 우선순위를 갖도록 설정
         }
     }
+    static int N, M, C; //도시의 개수, 통로의 개수, 시작 노드
+    static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>(); //각 노드에 인접한 노드에 대한 정보
+    static int[] d = new int[30001]; //최단거리 저장
 
-    static int findParent(int x){ //부모 노드를 찾기(루트노드일 때까지)
-        if(x == parent[x]) return x; //루트노드라면
-        return parent[x] = findParent(parent[x]);
-    }
+    static void dijkstra(int start){ //start 노드로부터의 거리 다익스트라 알고리즘
+        PriorityQueue<Node> pq = new PriorityQueue<>();
 
-    static void union(int a, int b){ //두 노드 연결하기
-        int na = findParent(a);
-        int nb = findParent(b);
+        //자기 자신까지의 거리 초기화
+        pq.offer(new Node(start, 0));
+        d[start] = 0;
 
-        if(na<nb) parent[nb] = parent[na];
-        else parent[na] = parent[nb];
+        while(!pq.isEmpty()){ //큐가 빌 때까지 반복
+            Node node = pq.poll();
+            int now = node.getIndex(); //가장 작은 값의 노드
+            int dist = node.getDistance(); //가장 작은 값의 비용
+
+            if(d[now] < dist) continue;
+
+            for(int i=0; i<graph.get(now).size(); i++){
+                int cost = d[now] + graph.get(now).get(i).getDistance(); //나를 거쳐 다른 노드로 이동하는 거리
+
+                if(cost < d[graph.get(now).get(i).getIndex()]){ //나를 거쳐가는 값이 더 작다면
+                    d[graph.get(now).get(i).getIndex()] = cost; //최단거리 교체
+                    pq.offer(new Node(graph.get(now).get(i).getIndex(), cost)); //우선순위 큐에 삽입
+                }
+            }
+
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt(); //집의 개수
-        int M = sc.nextInt(); //길의 개수
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int C = sc.nextInt();
 
-        Edge[] edge = new Edge[M]; //간선의 정보
-        for(int i=0; i<M; i++){
-            edge[i] = new Edge(sc.nextInt(), sc.nextInt(), sc.nextInt());
+        //최단거리 초기화
+        for(int i=0; i<=N; i++){
+            Arrays.fill(d, Integer.MAX_VALUE);
         }
 
-        //부모 노드 자기 자신으로 초기화
-        parent = new int[N+1];
+        //그래프 초기화
+        for(int i=0; i<=N; i++){
+            graph.add(new ArrayList<Node>());
+        }
+
+        for(int i=0; i<M; i++){ //간선 정보 입력
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            int Z = sc.nextInt();
+
+            graph.get(X).add(new Node(Y, Z));
+        }
+
+        dijkstra(C); //C 로부터 최단거리 다익스트리 알고리즘
+
+        int city = 0; //C가 보낼 수 있는 도시의 개수
+        int time = 0; //C가 보내는 데 걸리는 시간
+
         for(int i=1; i<=N; i++){
-            parent[i] = i;
+            //C가 도달할 수 없거나 자기 자신이면 무시
+            if(d[i] == Integer.MAX_VALUE || d[i] == 0) continue;
+
+            //아니라면
+            city++; //도시 수 증가
+            time = Math.max(time, d[i]); //가능한 도시 중 최대 시간 저장
         }
 
-        //간선(길)을 비용이 적은 순으로 정렬
-        Arrays.sort(edge, new Comparator<Edge>() {
-            @Override
-            public int compare(Edge o1, Edge o2) {
-                return o1.cost - o2.cost;
-            }
-        });
-
-        int result = 0; //비용 최솟값 결과
-        int last = 0;   //최소 비용 트리에서 제일 비싼 간선 비용
-
-        for(int i=0; i<M; i++){
-            //연결된 두 집의 부모가 같지 않을 때만
-            if(findParent(edge[i].getHome1()) != findParent(edge[i].getHome2())){
-                union(edge[i].getHome1(), edge[i].getHome2()); //두 노드 연결하기
-                result = result + edge[i].getCost(); //비용 더하기
-                last = edge[i].getCost(); //비용순으로 edge가 정렬되었기 때문에 루프가 끝난 후 제일 비싼 값이 저장됨
-            }
-        }
-
-        System.out.println(result - last); //최소비용 트리에서 제일 비싼 비용을 빼면, 두 개로 분리됨
+        System.out.println(city + " " + time);
     }
+
 }
